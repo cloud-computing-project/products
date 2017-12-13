e5145101b8976286792365e7fc0b31ebe9e4b2a6
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2017-12-13 04:44:35 +0100

    added api

---
+++ api/src/main/java/rso/project/products/api/v1/ProductApplication.java
@@ -0,0 +1,8 @@
+package rso.project.products.api.v1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1")
+public class ProductApplication extends Application{
+}
