499e64158c8896acdd82ed7ef3a5a46f86e783e4
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2017-12-13 11:54:19 +0100

    third try travis

--- models/src/main/resources/sql-scripts/init-products.sql
+++ models/src/main/resources/sql-scripts/init-products.sql
@@ -1,3 +1,3 @@
-INSERT INTO products (id, title) VALUES (1, 'Keyboard',);
+INSERT INTO products (id, title) VALUES (1, 'Keyboard');
 INSERT INTO products (id, title) VALUES (2, 'Mouse');
 INSERT INTO products (id, title) VALUES (3, 'Monitor');
