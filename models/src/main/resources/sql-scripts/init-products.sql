e4883ba8c1ea6d0ad7766365d5b88bd7824842af
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2017-12-13 05:32:38 +0100

    added kubernetes files

--- models/src/main/resources/sql-scripts/init-products.sql
+++ models/src/main/resources/sql-scripts/init-products.sql
@@ -1,3 +1,3 @@
 INSERT INTO customer (id, product_name, description, price, order_id) VALUES (1, 'Coffee Mug', 'Coffee Mug with characters from Star Wars', '20', 1);
 INSERT INTO customer (id, product_name, description, price, order_id) VALUES (2, 'Blue Jeans', 'Pepe Blue Jeans', '200', 1);
-INSERT INTO customer (id, product_name, description, price, order_id) VALUES (1, 'Desk Lamp', 'Wide desk lamp with three levels of lightning', '50', 1);
+INSERT INTO customer (id, product_name, description, price, order_id) VALUES (3, 'Desk Lamp', 'Wide desk lamp with three levels of lightning', '50', 1);
