b4630c17a90ca7ad424a267dda0cdad2e656f2cf
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2018-01-07 17:50:32 +0100

    added build number and sql script

--- models/src/main/resources/sql-scripts/init-products.sql
+++ models/src/main/resources/sql-scripts/init-products.sql
@@ -1,6 +1,6 @@
-INSERT INTO products (id, title) VALUES (1, 'T-Shirt');
-INSERT INTO products (id, title) VALUES (2, 'Basketball');
-INSERT INTO products (id, title) VALUES (3, 'Star Wars Mask');
-INSERT INTO products (id, title) VALUES (4, 'Jeans');
-INSERT INTO products (id, title) VALUES (5, 'Sunglasses');
-INSERT INTO products (id, title) VALUES (6, 'Umbrella');
+INSERT INTO products (id, title, manufacturer_id, category_id, price) VALUES (1, 'T-Shirt',1,1,30);
+INSERT INTO products (id, title, manufacturer_id, category_id, price) VALUES (2, 'Basketball',2,2,50);
+INSERT INTO products (id, title, manufacturer_id, category_id, price) VALUES (3, 'Star Wars Mask',1,2,25);
+INSERT INTO products (id, title, manufacturer_id, category_id, price) VALUES (4, 'Jeans',1,1,40);
+INSERT INTO products (id, title, manufacturer_id, category_id, price) VALUES (5, 'Sunglasses',3,1,100);
+INSERT INTO products (id, title, manufacturer_id, category_id, price) VALUES (6, 'Umbrella',3,3,15);
